a8d95eead1fd6536c64ef50d6788940e32c19281
Rouksana <rouksana.shannon@gmail.com>
2014-10-17 11:01:57 +0200

    feature/rouksana add event controller

---
+++ app/controllers/api/admin/events_controller.rb
@@ -0,0 +1,13 @@
+class Api::Admin::EventsController < Api::Admin::AdminController
+    def index
+    end
+    def create
+    	render json: false
+    end
+
+    def edit
+    end
+
+    def destroy
+    end
+end
